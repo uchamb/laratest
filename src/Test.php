47d1fafc2cc373a24be9493804945b4f1f968b5a
Ucha <ucha4964@gmail.com>
2017-01-27 17:14:31 +0400

    Autoload

---
+++ src/Test.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Larkhana;
+
+class Test {
+  public function version() {
+    $version = "1.0.0";
+    echo "Laratest works fine. v".$version;
+  }
+}
--- src/test.php
+++
@@ -1,5 +0,0 @@
-<?php
-function laratest() {
-  $version = "0.0.1";
-  echo "Laratest works fine. v".$version;
-}
